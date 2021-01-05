3170c759a405425030cf1d3a247a2a3f1e3ffcc0
Nate Voigt <voigtnathan23@gmail.com>
2021-01-05 10:48:06 -0600

    added build file for aws restAPI

---
+++ build/precache-manifest.5ae487f2805be41e041d598743084f82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4bea94167d6693e4117db24f5a817a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cad1646623542c5e061",
+    "url": "/static/css/main.2b01f072.chunk.css"
+  },
+  {
+    "revision": "1fcc00b6926e5d359534",
+    "url": "/static/js/2.7d19a02f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.7d19a02f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cad1646623542c5e061",
+    "url": "/static/js/main.da922098.chunk.js"
+  },
+  {
+    "revision": "81a50874321991da4aaa",
+    "url": "/static/js/runtime-main.e8ea0e83.js"
+  }
+]);
